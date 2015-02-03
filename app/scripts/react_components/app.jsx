6fa3564abf9971723c5129179b1416dab3bc4381
Miguel Bermudez <miguel.bermudez@controlgroup.com>
2015-02-03 17:05:45 -0500

    use s3 url

--- app/scripts/react_components/app.jsx
+++ app/scripts/react_components/app.jsx
@@ -4,7 +4,7 @@ var React   = require('react');
 var PxImage = require('./pxImage.jsx');
 
 React.renderComponent(
-  <PxImage steps="2" min="1" max="50" width="760" height="428" url="images/DP220030.jpg" />,
+  <PxImage steps="2" min="1" max="50" width="760" height="428" url="https://s3.amazonaws.com/graphmet/original/DP220030.jpg" />,
   document.getElementById('demo')
 );
 
